a965851db89c661c8b1169df45f565253afabfef
ada173 <101599447+zoey-t@users.noreply.github.com>
2022-04-06 23:49:30 -0700

    finish the script for sourcify

--- src/soucify.ts
+++ src/soucify.ts
@@ -1,120 +1,187 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from 'axios';
 import FormData from 'form-data';
-import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import chalk from 'chalk';
 import fs from 'fs-extra';
 import path from 'path';
-import {Readable} from 'stream';
+import { Readable } from 'stream';
+import hre from "hardhat";
+import { getFullyQualifiedName } from "hardhat/utils/contract-names";
 
 function log(...args: any[]) {
-  console.log(...args);
+    console.log(...args);
 }
 
 function logError(...args: any[]) {
-  console.log(chalk.red(...args));
+    console.log(chalk.red(...args));
 }
 
 function logInfo(...args: any[]) {
-  console.log(chalk.yellow(...args));
+    console.log(chalk.yellow(...args));
 }
 
 function logSuccess(...args: any[]) {
-  console.log(chalk.green(...args));
+    console.log(chalk.green(...args));
 }
 
 function ensureTrailingSlash(s: string): string {
-  const lastChar = s.substr(-1);
-  if (lastChar != '/') {
-    s = s + '/';
-  }
-  return s;
+    const lastChar = s.substr(-1);
+    if (lastChar != '/') {
+        s = s + '/';
+    }
+    return s;
 }
 
 // const defaultEndpoint = 'https://server.verificationstaging.shardlabs.io/';
 const defaultEndpoint = 'https://sourcify.dev/server/';
 
+
 export async function submitSourcesToSourcify(
-  hre: HardhatRuntimeEnvironment,
-  config?: {
-    endpoint?: string;
-    contractName?: string;
-    writeFailingMetadata?: boolean;
-  }
+    hre: HardhatRuntimeEnvironment,
+    config: {
+        endpoint?: string;
+        sourceName: string; //path ./contracts/Greeter.sol
+        contractName: string; //name Greeter
+        address: string;
+        chainId: number
+
+
+        // address
+        // writeFailingMetadata?: boolean;
+    }
 ): Promise<void> {
-  config = config || {};
-  const chainId = await hre.getChainId();
-  const all = await hre.deployments.all();
-  const url = config.endpoint
-    ? ensureTrailingSlash(config.endpoint)
-    : defaultEndpoint;
+    config = config || {};
+    //   get chainId
+    const chainId = config.chainId
 
-  async function submit(name: string) {
-    const deployment = all[name];
-    const {address, metadata: metadataString} = deployment;
 
-    try {
-      const checkResponse = await axios.get(
-        `${url}checkByAddresses?addresses=${address.toLowerCase()}&chainIds=${chainId}`
-      );
-      const {data: checkData} = checkResponse;
-      if (checkData[0].status === 'perfect') {
-        log(`already verified: ${name} (${address}), skipping.`);
-        return;
-      }
-    } catch (e) {
-      logError(
-        ((e as any).response && JSON.stringify((e as any).response.data)) || e
-      );
+
+
+
+
+
+    //   const chainId = await hre.getChainId();
+    //   const all = await hre.deployments.all();
+    const url = config.endpoint
+        ? ensureTrailingSlash(config.endpoint)
+        : defaultEndpoint;
+
+    async function submit(name: string) {
+        // get chosenContract index
+        /// get chosenContract (contract index in hardhat metadata file)
+        const fullyQualifiedName = getFullyQualifiedName(config.sourceName, config.contractName)
+        const buildinfo = await hre.artifacts.getBuildInfo(fullyQualifiedName)
+        /// get contract index from output of buildinfo
+        let index;
+        if (buildinfo) {
+            index = getObjectKeyIndex(buildinfo.output.contracts, config.sourceName)
+            // index = Object.keys(buildinfo.output.contracts).indexOf(config.sourceName)
+            console.log("chosen contract", index)
+        } else {
+            // throw error
+        }
+        // const deployment = all[name];
+
+        // const {address, metadata: metadataString} = deployment;
+        const address = config.address
+        const metadataString = JSON.stringify(buildinfo)
+
+        try {
+            const checkResponse = await axios.get(
+                `${url}checkByAddresses?addresses=${address.toLowerCase()}&chainIds=${chainId}`
+            );
+            const { data: checkData } = checkResponse;
+            console.log(checkData[0].status);
+            if (checkData[0].status === 'perfect') {
+                log(`already verified: ${name} (${address}), skipping.`);
+                return;
+            }
+        } catch (e) {
+            logError(
+                ((e as any).response && JSON.stringify((e as any).response.data)) || e
+            );
+        }
+
+        if (!metadataString) {
+            logError(
+                `Contract ${name} was deployed without saving metadata. Cannot submit to sourcify, skipping.`
+            );
+            return;
+        }
+
+        logInfo(`verifying ${name} (${address} on chain ${chainId}) ...`);
+
+        const formData = new FormData();
+        formData.append('address', address);
+        formData.append('chain', chainId);
+        formData.append('chosenContract', index)
+
+        const fileStream = new Readable();
+        fileStream.push(metadataString);
+        fileStream.push(null);
+        formData.append('files', fileStream, 'metadata.json');
+
+        // console.log(formData);
+
+        try {
+            const submissionResponse = await axios.post(url, formData, {
+                headers: formData.getHeaders(),
+            });
+            if (submissionResponse.data.result[0].status === 'perfect') {
+                logSuccess(` => contract ${name} is now verified`);
+            } else {
+                logError(` => contract ${name} is not verified`);
+            }
+        } catch (e) {
+            //   if (config && config.writeFailingMetadata) {
+            //     const failingMetadataFolder = path.join('failing_metadata', "3");
+            //     fs.ensureDirSync(failingMetadataFolder);
+            //     fs.writeFileSync(
+            //       path.join(failingMetadataFolder, `${name}_at_${address}.json`),
+            //       metadataString
+            //     );
+            //   }
+            logError(
+                ((e as any).response && JSON.stringify((e as any).response.data)) || e
+            );
+        }
     }
 
-    if (!metadataString) {
-      logError(
-        `Contract ${name} was deployed without saving metadata. Cannot submit to sourcify, skipping.`
-      );
-      return;
+    if (config.contractName) {
+        await submit(config.contractName);
+    } else {
+        // await submit("GreeterA");
+        // for (const name of Object.keys(all)) {
+        //   await submit(name);
+        // }
     }
 
-    logInfo(`verifying ${name} (${address} on chain ${chainId}) ...`);
 
-    const formData = new FormData();
-    formData.append('address', address);
-    formData.append('chain', chainId);
 
-    const fileStream = new Readable();
-    fileStream.push(metadataString);
-    fileStream.push(null);
-    formData.append('files', fileStream, 'metadata.json');
-
-    try {
-      const submissionResponse = await axios.post(url, formData, {
-        headers: formData.getHeaders(),
-      });
-      if (submissionResponse.data.result[0].status === 'perfect') {
-        logSuccess(` => contract ${name} is now verified`);
-      } else {
-        logError(` => contract ${name} is not verified`);
-      }
-    } catch (e) {
-      if (config && config.writeFailingMetadata) {
-        const failingMetadataFolder = path.join('failing_metadata', chainId);
-        fs.ensureDirSync(failingMetadataFolder);
-        fs.writeFileSync(
-          path.join(failingMetadataFolder, `${name}_at_${address}.json`),
-          metadataString
-        );
-      }
-      logError(
-        ((e as any).response && JSON.stringify((e as any).response.data)) || e
-      );
-    }
-  }
-
-  if (config.contractName) {
-    await submit(config.contractName);
-  } else {
-    for (const name of Object.keys(all)) {
-      await submit(name);
-    }
-  }
 }
+
+function getObjectKeyIndex(obj: any, keyToFind: any) {
+    var i = 0, key;
+
+    for (key in obj) {
+        if (key == keyToFind) {
+            return i;
+        }
+
+        i++;
+    }
+
+    return i;
+}
+
+submitSourcesToSourcify(hre, {
+
+    sourceName: "contracts/Greeter.sol", //path ./contracts/Greeter.sol
+    address: "0x122345454646456546654",
+    contractName: "Greeter",
+    chainId: 3
+}).catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
